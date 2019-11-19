fadaeb051ba72025c5dfaf84462e0d184a7efc9e
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2019-11-19 17:18:50 +0200

    1

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const del = require('del');
 const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
+const uglify = require('gulp-uglify-es').default;
 sass.compiler = require('node-sass');
 const sassPaths = ['./node_modules'];
 
@@ -47,23 +47,19 @@ gulp.task('imagemin', function() {
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'));
 });
-// gulp.task('script', function() {
-//   return gulp
-//     .src([
-//       // './node_modules/slick-carousel/slick/slick.min.js',
-//       './app/js/main.js',
-//     ])
-//     .pipe(concat('script.js'))
-//     .pipe(gulp.dest('./dist/js/'))
-//     .pipe(
-//       uglify({
-//         toplevel: true,
-//       }),
-//     )
-//     .pipe(rename('main.min.js'))
-//     .pipe(gulp.dest('./dist/js'))
-//     .pipe(browserSync.stream());
-// });
+gulp.task('script', function() {
+  return gulp
+    .src([
+      // './node_modules/slick-carousel/slick/slick.min.js',
+      './app/js/main.js',
+    ])
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('./dist/js/'))
+    .pipe(uglify())
+    .pipe(rename('main.min.js'))
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(browserSync.stream());
+});
 // эта функцию можно убарать поставить с вотчере gulp.watch('./*.html').on('change', browserSync.reload); если нам не нужно делать чтото с .html
 
 // function html() {
@@ -86,7 +82,7 @@ gulp.task('server', function() {
     },
   });
 
-  //gulp.watch('./app/js/main.js', gulp.series('script'));
+  gulp.watch('./app/js/main.js', gulp.series('script'));
   gulp.watch('./app/css/*.scss', gulp.series('do_css'));
   // gulp.watch('./*.html', gulp.series(html));
   gulp.watch('./*.html').on('change', browserSync.reload);
@@ -98,7 +94,7 @@ gulp.task('watch', function() {
 // важно запускаем после вотчера этот таck
 
 // // =======собираем наши таски gulp scriptи do_css  и  в один таск
-gulp.task('bild', gulp.series('del', gulp.parallel('do_css', 'imagemin')));
+gulp.task('bild', gulp.series('del', gulp.parallel('do_css','script', 'imagemin')));
 gulp.task('dev', gulp.series('bild', 'server'));
 
 gulp.task('default', gulp.series('dev'));
